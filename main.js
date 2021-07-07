458f30bc691c14bff7b673f3e8d41385ed21b73e
Julian Nunes <contato@juliannunes.com.br>
2021-07-07 20:40:08 -0300

    Desafio 2 - Criando o componente dos títulos

--- main.js
+++ main.js
@@ -3,13 +3,16 @@ import './src/styles/generic/reset.css'; // reset padrão do Eric Meyers
 import './src/styles/elements/base.css';
 
 import CardGame from './src/components/CardGame';
+import PlayerTitle from './src/components/PlayersTitle'
 
 const $root = document.querySelector('#root');
 let $htmlCardGame = CardGame();
+const $players = PlayerTitle(1) + PlayerTitle(2)
 
 for(let i = 0; i<5;i++){
     $htmlCardGame += CardGame() // podemos concatenar porque o CardGame retorna uma string
 }
 
+$root.insertAdjacentHTML('beforebegin',$players);
 $root.insertAdjacentHTML('afterbegin',$htmlCardGame);
 
---
+++ src/components/PlayersTitle/index.js
@@ -0,0 +1,9 @@
+import './style.css'
+
+export default function(n){
+    return /*html*/`
+    <h3>
+        Player ${n}
+    </h3>
+    `
+}
